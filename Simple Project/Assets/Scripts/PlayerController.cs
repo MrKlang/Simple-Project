a271bff1462587b2f4d97d00f9f1f11d563ccbe4
MrKlang_Spare <orogow@gmail.com>
2019-03-15 20:24:45 +0100

    Initial Commit
    Half of a level is complete (corridor,steps).
    Player and its obligatory controlls (WSAD/arrow movement, mouse rotation and jump) are programmed.

---
+++ Simple Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float OriginalSpeed;
+    public Rigidbody RBody;
+
+    private bool IsJumping;
+    private float Speed;
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            Speed = OriginalSpeed*3f;
+        }
+        else
+        {
+            Speed = OriginalSpeed;
+        }
+
+        if (Input.GetAxis("Vertical") != 0)
+        {
+            var verticalMovement = Input.GetAxis("Vertical") * Speed * Time.deltaTime;
+            transform.Translate(new Vector3(-verticalMovement, 0, 0));
+        }
+
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+            var horizontalStrafe = Input.GetAxis("Horizontal") * Speed* Time.deltaTime;
+            transform.Translate(new Vector3(0, 0, horizontalStrafe));
+        }
+
+        if(Input.GetAxis("Mouse X") != 0)
+        {
+            var rotation = Input.GetAxis("Mouse X");
+            transform.localEulerAngles += new Vector3(0,rotation,0);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && !IsJumping)
+        {
+            RBody.AddRelativeForce(new Vector3(0, 300, 0),ForceMode.Impulse);
+            IsJumping = true;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Floor") && IsJumping)
+        {
+            IsJumping = false;
+        }
+    }
+}
